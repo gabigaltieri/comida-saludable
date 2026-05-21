5858fe91d1d2b3afc4d6af027265255559651c1c
gabigaltieri <gabrielgaltieri2@gmail.com>
2026-05-21 15:59:57 -0300

    fix: use trimmed env vars in supabase browser client

--- src/lib/supabase-browser.ts
+++ src/lib/supabase-browser.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export function createSupabaseBrowser() {
   return createClient(
-    "https://sdlgfdmhasftbelztvqb.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkbGdmZG1oYXNmdGJlbHp0dnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxOTgwODcsImV4cCI6MjA5Mzc3NDA4N30.toPTAtDrimueyJW01DXUg8fubA-QpbZno8zxcPFLy00"
+    process.env.NEXT_PUBLIC_SUPABASE_URL!.trim(),
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!.trim()
   );
 }
